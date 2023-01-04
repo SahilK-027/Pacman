fe38ad0f77e7076999744645269029e371546443
Sahil Kandhare <sahilkandhare07@gmail.com>
2023-01-04 11:52:16 +0530

    :)

--- index.js
+++ index.js
@@ -12,20 +12,12 @@ const scoreEl = document.getElementById('scoreEl')
  */
 const sizes = {
     width: window.innerWidth,
-    height: window.innerHeight
+    height: 820
 }
 // Resize window
 window.addEventListener('resize',()=>{
     // Update sizes
     sizes.width = window.innerWidth;
-    sizes.height =  window.innerHeight;
-    // Update camera aspect
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 canvas.width = sizes.width
